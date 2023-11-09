18b7a2925999de6b7d6368f54eb1d345b990eee4
JasonLee-p <2593292614@qq.com>
2023-11-09 19:17:45 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+VERSION = 1
+
+
+setuptools.setup(
+    name="pypcs",
+    version='0.0.' + str(VERSION),
+    author="Jason Lee",
+    author_email="2593292614@qq.com",
+    description="",
+    long_description=open('README.md', 'r', encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    install_package_data=True,
+    package_data={'': ['*.txt', '*.md', '*.json', '*.png']},
+    url="https://github.com/JasonLee-p/pyPCS",
+    install_requires=[
+        'numpy==1.24.2',
+        'pygame==2.3.0',
+        'rtmidi==2.5.0',
+        'webcolors==1.12',
+        'Pillow==9.5.0'
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+    ],
+)
